208f7b49e3aa4d284a56757a6544fa12365a73c2
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-05-09 11:05:59 +0900

    기존 스타일대로 인스턴스 생성, 초기화

---
+++ InstanceVariablePrac/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace InstanceVariablePrac
+{
+    internal class Product
+    {
+        public string name;
+        public int price;
+    }
+}
---
+++ InstanceVariablePrac/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstanceVariablePrac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+
+            // 자바 스타일
+            product1.name = "감자";
+            product1.price = 2000;
+        }
+    }
+}
