66b80b9dfc421fa0ed928b3feebd96e02c8337b4
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-05-09 11:10:39 +0900

    C# 스타일로 인스턴스 변수 초기화하기

--- InstanceVariablePrac/Program.cs
+++ InstanceVariablePrac/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,12 @@ namespace InstanceVariablePrac
             // 자바 스타일
             product1.name = "감자";
             product1.price = 2000;
+
+            // C# 스타일
+            Product product2 = new Product() { name = "고구마", price = 2100 };
+            Product product3 = new Product() { name = "옥수수" };
+            Product product4 = new Product() { price = 999999 };
+            Product products = new Product() { price = 7777, name = "비트" };
         }
     }
 }
